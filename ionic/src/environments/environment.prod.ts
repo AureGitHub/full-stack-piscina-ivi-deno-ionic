74f8db3f340d9d4403b101111cfd5b12e56f8134
AureGitHub <auredeveloper@gmail.com>
2024-07-10 14:05:56 +0200

    build ionic

--- ionic/src/environments/environment.prod.ts
+++ ionic/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  server: 'https://auregithub-bar-fay-ful-67.deno.dev/',  
+  server: 'https://piscina-ivi.deno.dev/',  
   key_user: 'e12b1871-c221-448b-9089-71751478793d',
   key_app: 'cf13d09a-0a8a-11ee-be56-0242ac120002',
   localStorageSave_secret: '971f8ad8-00c9-4e8a-9f8a-36dcd42cd8bf',
